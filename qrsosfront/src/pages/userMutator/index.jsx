6bc3da24f1c11067d2d40e59630ec6f786299b26
CorradoDeLuca <corrado.deluca@bubblebpm.com>
2020-12-27 05:08:33 -0300

    Modifies code in staff

--- qrsosfront/src/pages/userMutator/index.jsx
+++ qrsosfront/src/pages/userMutator/index.jsx
@@ -1,8 +1,27 @@
-import { TextField, Typography, Divider, Grid, InputAdornment } from '@material-ui/core';
+import { TextField, Typography, Divider, Grid, InputAdornment, Card, IconButton } from '@material-ui/core';
 import DoneIcon from '@material-ui/icons/Done';
 import { useState } from 'react';
 import Loader from "../../components/Loader"
 import {USER_QUERY} from "../../containers/User/queries/userDataQUery"
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+
+function CustomInput(props){
+    const handleChange = (e,f) => {
+        f(e.currentTarget.value)
+    }
+    return(
+        <TextField
+        onChange={ e => handleChange(e,props.f)}
+        label={props.label}
+        defaultValue={props.defaultValue}
+        InputProps={{
+        endAdornment: (
+            <InputAdornment position="end">
+            <DoneIcon color={props.state ? "primary" : "dafault"} />
+            </InputAdornment>
+        ),}}/>      
+    )
+}
 
 function UserEditor(props){
     return (
@@ -18,38 +37,33 @@ function UserMutator(props){
     const [name, setName] = useState(props.user.firstName)
     const [lastName, setLastName] = useState(props.user.lastName)
 
-    const handleChange = (e,f) => {
-        f(e.currentTarget.value)
-    }
     return(
         <div>
             <center><Typography variant="h5">Editar Perfil</Typography></center>
             <Divider />
             <Grid  container style={{margin:"2%"}}>
                 <Grid style={{margin:"2%"}} item xs={5}>
-                <TextField
-                    onChange={ e => handleChange(e,setName)}
-                    label="Nombre"
-                    defaultValue={props.user.firstName}
-                    InputProps={{
-                    endAdornment: (
-                        <InputAdornment position="end">
-                        <DoneIcon color={name ? "primary" : "dafault"} />
-                        </InputAdornment>
-                    ),}}/>                
+                    <Card style={{backgroundColor:"#d4d0c5", height:200,width:160}}>
+                        <IconButton style={{color:"white"}}><AddAPhotoIcon /></IconButton>
+                    </Card>
                 </Grid>
-                <Grid style={{margin:"2%"}} item xs={5}>
-                <TextField
-                    onChange={ e => handleChange(e,setLastName)}
-                    label="Nombre"
-                    defaultValue={props.user.lastName}
-                    InputProps={{
-                    endAdornment: (
-                        <InputAdornment position="end">
-                        <DoneIcon color={lastName ? "primary" : "dafault"} />
-                        </InputAdornment>
-                    ),}}/>                
+                <Grid style={{margin:"2%"}} container xs={5}>
+                    <Grid style={{margin:"2%"}} item xs={6}>
+                    <CustomInput
+                        f={setName}
+                        label="Nombre"
+                        defaultValue={props.user.firstName}
+                        state={name}/>                
+                    </Grid>
+                    <Grid style={{margin:"2%"}} item xs={6}>
+                    <CustomInput
+                        f={setLastName}
+                        label="Apellido"
+                        defaultValue={props.user.lastName}
+                        state={lastName}/>                
+                    </Grid>
                 </Grid>
+
             </Grid>
 
         </div>
